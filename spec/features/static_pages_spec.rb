04074cab6d3bae8c35fcfb4a8ef23a2d84abbb87
sgodithi <sunil@ameya.com>
2014-03-18 22:31:30 -0700

    After Excercise One in Section 3.5

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -28,6 +28,19 @@ describe "Static pages" do
     end
   end
 
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+  
   describe "About page" do
 
     it "should have the content 'About Us'" do
