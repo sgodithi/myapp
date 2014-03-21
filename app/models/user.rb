75f55de4d22645285153fe1be94ef6e502a5df97
sgodithi <sunil@ameya.com>
2014-03-20 23:55:36 -0700

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # callback for downcasing email addresses
   before_save { email.downcase! }
+  before_create :create_remember_token
   
   # validations
   validates(:name, presence: true,length: { maximum: 50 })
@@ -9,5 +10,19 @@ class User < ActiveRecord::Base
   
   has_secure_password
   validates :password, length: { minimum: 6 }
+  
+  def User.new_remember_token
+      SecureRandom.urlsafe_base64
+  end
+
+  def User.hash(token)
+      Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+        self.remember_token = User.hash(User.new_remember_token)
+    end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,14 @@ Myapp::Application.routes.draw do
   # get "static_pages/contact"
   # get "static_pages/about"
   
+  
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
