f91725923d2d6c52c9ab411fdb2e43cba21622e5
sgodithi <sunil@ameya.com>
2014-03-21 14:25:41 -0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
   
   def show
-    @user =User.find(params[:id])
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def index
@@ -60,11 +61,6 @@ class UsersController < ApplicationController
       end
       
       # Before filters
-
-      def signed_in_user
-        store_location
-        redirect_to signin_url, notice: "Please sign in." unless signed_in?
-      end
       
       def correct_user
         @user = User.find(params[:id])
